e85809f6450af001690b5e7471a4a7bd895aeb0e
holyboom1 <lesha744@gmail.com>
2020-06-23 09:53:38 +0300

    задание со звездами с изменениями

--- js/3/jscript/js.js
+++ js/3/jscript/js.js
@@ -5,17 +5,23 @@ var str = "";
 var k=0;
 var kk=0;
     if (!isNaN(x)) {
-        kk=x;
-        do {
-            var kol=0;
-            do{
-                str += "*"
-                kol++
-            }while (kol<kk)
-            str += "\n";
-        k++
-        kk--
-        } while (k<x)
+        if (x==0){
+            console.log ("вы ввели ноль");
+        }
+        if (x<0){console.log("число меньше нуля")}
+        if (x>0) {
+            kk = x;
+            do {
+                var kol = 0;
+                do {
+                    str += "*"
+                    kol++
+                } while (kol < kk)
+                str += "\n";
+                k++
+                kk--
+            } while (k < x)
+        }
         }
     else {
         console.error("не число")
