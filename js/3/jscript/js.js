94ae5278969b6f2dda56edee23708426744f0047
holyboom1 <lesha744@gmail.com>
2020-06-23 09:48:00 +0300

    задание со звездами

--- js/3/jscript/js.js
+++ js/3/jscript/js.js
@@ -1,21 +1,23 @@
 'use strict'
 
-var x=100;
+var x=+prompt("введите число", 1);
 var str = "";
-
-    do {
-        var y=x
-        var ost=(y%10);
-        y=(y-ost)/10;
-        var ost2=(y%10);
-        y=(y-ost2)/10;
-        var ost3=(y%10)
-        if (ost+ost2+ost3 == 15)
-        {
-            str+=x+ " ";
+var k=0;
+var kk=0;
+    if (!isNaN(x)) {
+        kk=x;
+        do {
+            var kol=0;
+            do{
+                str += "*"
+                kol++
+            }while (kol<kk)
+            str += "\n";
+        k++
+        kk--
+        } while (k<x)
         }
-        x++
-
-        // kbhg66
-    }while (x<1000)
+    else {
+        console.error("не число")
+    }
 console.log(str);
