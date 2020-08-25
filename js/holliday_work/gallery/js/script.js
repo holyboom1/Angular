1d3aa787a0630e90706859d54fc5a9fc32b69a3e
Home pc <lesha744@gmail.com>
2020-08-25 16:33:14 +0300

    фывф

--- js/holliday_work/gallery/js/script.js
+++ js/holliday_work/gallery/js/script.js
@@ -1,108 +1,142 @@
-//  'use strict';
-//  window.onload = function() {
-//
-//      let closeModel = document.getElementsByClassName("closeModel");
-//      closeModel[0].addEventListener("click", closeFn);
-//
-//      function closeFn() {
-//          document.getElementsByClassName("selectElem show_item")[0].className = "selectElem hide_item"
-//          document.body.className = "unSelect"
-//          document.getElementsByClassName("gallery hide_item")[0].className = "gallery show_item"
-//
-//
-//      }
-//
-//      let divSelect = document.getElementsByClassName("main");
-//      divSelect[0].addEventListener("click", clickOnImg);
-//
-//      let gallery = document.getElementsByClassName("gallery")[0];
-//      let li = gallery.getElementsByTagName("li");
-//      let images = gallery.children[1].getElementsByTagName("img");
-//      let img = gallery.getElementsByClassName("main")[0].children[0];
-//
-//      for (let i = 0; i < images.length; i++) {
-//          images[i].addEventListener("click", itemClick);
-//      }
-//      document.getElementsByClassName("nextImg")[0].addEventListener("click", nextImg)
-//       document.getElementsByClassName("prevImg")[0].addEventListener("click", prevImg);
-//
-//      init(0);
-//
-//      function clickOnImg() {
-//          show_select_item.setAttribute("src", select_item.getAttribute("src"))
-//          document.getElementsByClassName("selectElem hide_item")[0].className = "selectElem show_item"
-//          document.body.className = ""
-//          document.getElementsByClassName("gallery show_item")[0].className = "gallery hide_item"
-//          document.getElementsByClassName("textItem")[0].innerText = document.getElementById("text").innerText
-//
-//      }
-//
-//      function itemClick() {
-//          img.setAttribute("src", this.getAttribute("src"))
-//          document.getElementById("text").innerHTML = this.getAttribute("alt");
-//          document.getElementsByClassName("gallery")[0].getElementsByClassName("active")[0].removeAttribute("class");
-//
-//          this.className = "active";
-//      }
-//
-//      function init(index) {
-//          //img.src = images[index].src;
-//          img.setAttribute("src", images[index].getAttribute("src"));
-//          document.getElementById("text").innerHTML = images[index].getAttribute("alt");
-//          images[index].setAttribute("class", "active")
-//      }
-//
-// //
-//
-//
-//      function nextImg() {
-//          const active = document.querySelector(".activeLi");
-//          var nextImg=active.nextElementSibling;
-//          if (nextImg==null){
-//              document.getElementsByClassName("active")[0].removeAttribute("class");
-//              document.getElementsByClassName("activeLi")[0].removeAttribute("class");
-//              show_select_item.setAttribute("src", images[0].getAttribute("src"));
-//              document.getElementsByClassName("textItem")[0].innerText = images[0].getAttribute("alt");
-//              document.getElementById("select_item").setAttribute("src", images[0].getAttribute("src"));
-//              document.getElementById("text").innerText = images[0].getAttribute("alt");
-//              images[0].setAttribute("class", "active");
-//              li[0].setAttribute("class", "activeLi");
-//          }
-//          else {
-//              document.getElementsByClassName("active")[0].removeAttribute("class");
-//              document.getElementsByClassName("activeLi")[0].removeAttribute("class");
-//              nextImg.getElementsByTagName("img")[0].setAttribute("class", "active");
-//              nextImg.setAttribute("class", "activeLi");
-//              show_select_item.setAttribute("src", nextImg.getElementsByTagName("img")[0].getAttribute("src"));
-//              document.getElementsByClassName("textItem")[0].innerText = nextImg.getElementsByTagName("img")[0].getAttribute("alt")
-//              document.getElementById("select_item").setAttribute("src", nextImg.getElementsByTagName("img")[0].getAttribute("src"));
-//              document.getElementById("text").innerText = nextImg.getElementsByTagName("img")[0].getAttribute("alt");
-//          }
-//      }
-//
-//      function prevImg() {
-//          const active = document.querySelector(".activeLi");
-//          var nextImg=active.previousElementSibling;
-//          if (nextImg==null){
-//              document.getElementsByClassName("active")[0].removeAttribute("class");
-//              document.getElementsByClassName("activeLi")[0].removeAttribute("class");
-//              show_select_item.setAttribute("src", images[images.length-1].getAttribute("src"));
-//              document.getElementsByClassName("textItem")[0].innerText = images[images.length-1].getAttribute("alt");
-//              document.getElementById("select_item").setAttribute("src", images[images.length-1].getAttribute("src"));
-//              document.getElementById("text").innerText = images[images.length-1].getAttribute("alt");
-//              images[images.length-1].setAttribute("class", "active");
-//              li[images.length-1].setAttribute("class", "activeLi");
-//          }
-//          else {
-//              document.getElementsByClassName("active")[0].removeAttribute("class");
-//              document.getElementsByClassName("activeLi")[0].removeAttribute("class");
-//              nextImg.getElementsByTagName("img")[0].setAttribute("class", "active");
-//              nextImg.setAttribute("class", "activeLi");
-//              show_select_item.setAttribute("src", nextImg.getElementsByTagName("img")[0].getAttribute("src"));
-//              document.getElementsByClassName("textItem")[0].innerText = nextImg.getElementsByTagName("img")[0].getAttribute("alt")
-//              document.getElementById("select_item").setAttribute("src", nextImg.getElementsByTagName("img")[0].getAttribute("src"));
-//              document.getElementById("text").innerText = nextImg.getElementsByTagName("img")[0].getAttribute("alt");
-//          }
-//      }
-//
-//  }
+'use strict';
+window.onload = function() {
+    var imagesCount=111; // количество изображений в нашем архиве
+    var z=imagesCount/9;
+    var pageesCount=parseInt(z);
+    if (parseFloat(z)>0){
+        pageesCount+=1
+    }
+
+    let img = document.getElementsByClassName("img");
+    document.getElementById("nextPage").addEventListener("click",nextPage);
+    document.getElementById("prevPage").addEventListener("click", prevPage);
+    document.getElementById("close_button").addEventListener("click", closeFn);
+    document.getElementById("nextImg").addEventListener("click", nextImg)
+    document.getElementById("prevImg").addEventListener("click", prevImg);
+
+    for (let i=0; i<img.length; i++){
+        img[i].addEventListener("click",openImg);
+        img[i].setAttribute("src",addSrcImg(i))
+    }
+
+    function addSrcImg(i) {
+        var src=""
+        if (i>0) {
+            src += "images/img%20(" + (+(i) + 1) + ").jpg"
+        }
+        else {
+            src="images/img%20(1).jpg"
+        }
+            return src;
+    }
+
+    function openImg() {
+        if (this.getAttribute("src")=="img/lastimg.jpg"){}
+        else {
+            document.getElementById("show_select_item").setAttribute("src", this.getAttribute("src"))
+            document.getElementById("selectElem").classList.toggle("show_item")
+            this.classList.add("active")
+        }
+    }
+
+    function nextPage() {
+        let page = (+document.getElementById("page").innerText);
+        if (pageesCount==page){
+            console.log("last page");
+        }
+        else {
+            for (let i = 0; i < img.length; i++) {
+                img[i].setAttribute("src", addSrcImgNext(i))
+            }
+
+            function addSrcImgNext(i) {
+                let x = (page * 9) + 1;
+                if (x + i < imagesCount + 1) {
+                    var src = "";
+                    src += "images/img%20(" + (i + x) + ").jpg";
+                    return src;
+                } else {
+                    var src = "";
+                    src += "img/lastimg.jpg";
+                    return src;
+                }
+            }
+            document.getElementById("page").innerText=page+1
+        }
+    }
+
+    function prevPage() {
+        var pageNamber=(+document.getElementById("page").innerText)
+        if (pageNamber==1){
+                console.log("FIRST PAGE")
+        }
+        if(pageNamber==2){
+            for (let i = 0; i < img.length; i++) {
+                img[i].setAttribute("src", addSrcImgPrev(i))
+            }
+            function addSrcImgPrev(i) {
+                var src = "";
+                src += "images/img%20(" + (i + 1) + ").jpg";
+                return src;
+            }
+            document.getElementById("page").innerText = pageNamber-1
+        }
+        if (pageNamber>1){
+            for (let i = 0; i < img.length; i++) {
+                img[i].setAttribute("src", addSrcImgPrev1(i))
+            }
+            function addSrcImgPrev1(i) {
+                let x = ((pageNamber-2) * 9) + 1;
+                var src = "";
+                src += "images/img%20(" + (i + x) + ").jpg";
+                return src;
+
+            }
+            document.getElementById("page").innerText = pageNamber -1
+        }
+    }
+
+     function closeFn() {
+         document.getElementById("selectElem").classList.toggle("show_item")
+     }
+
+     function nextImg() {
+            var x=document.getElementById("show_select_item").getAttribute("src");
+            var z=x.split("images/img%20(");
+            var s=z+"";
+            z=s.split(").jpg");
+            s=z+"";
+            z=s.split(",")
+            if (z[1]<imagesCount) {
+                var src = "images/img%20(" + ((+z[1]) + 1) + ").jpg"
+                document.getElementById("show_select_item").setAttribute("src", src)
+                if ((((+z[1])+1)%9)==1){
+                    nextPage()
+                }
+            }
+            else {
+                console.log("end")
+            }
+
+     }
+
+     function prevImg() {
+         var x=document.getElementById("show_select_item").getAttribute("src");
+         var z=x.split("images/img%20(");
+         var s=z+"";
+         z=s.split(").jpg");
+         s=z+"";
+         z=s.split(",")
+         if (z[1]!=1) {
+             var src = "images/img%20(" + ((+z[1]) - 1) + ").jpg"
+             document.getElementById("show_select_item").setAttribute("src", src)
+             if ((((+z[1])-1)%9)==0){
+                 prevPage()
+             }
+         }
+         else {
+             console.log("start")
+         }
+     }
+
+}
