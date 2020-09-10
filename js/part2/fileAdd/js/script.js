551b2389c06cfdf7e7a327fab170e18f8e327130
IHOLY <LESHA744@GMAIL.COM>
2020-09-10 21:26:02 +0300

    QWE

--- js/part2/fileAdd/js/script.js
+++ js/part2/fileAdd/js/script.js
@@ -11,20 +11,48 @@ window.onload = function () {
         }return x;
     }
 
-        function addFile(e) {
-            e.preventDefault();
-            let delFile =  ce("img","","click",delFiles);
-            delFile.src="/img/close.png"
-            let addElem = ce("input");
-            addElem.type="file"
-            addElem.name=`File#${countFiles}`;
-            this.parentElement.append(addElem);
-            countFiles++
-            console.log(addElem)
+    function addFile(e) {
+        e.preventDefault();
+        let parent= ce("div");
+        let delFile =  ce("img");
+        delFile.addEventListener("click", delFiles)
+        delFile.setAttribute("src","img/close.png");
+        delFile.setAttribute("user-data",`File#${countFiles}`);
+
+        let addElem = ce("input");
+        addElem.type="file"
+        addElem.name=`File#${countFiles}`;
+        parent.append(delFile);
+        parent.append(addElem);
+        this.parentElement.append(parent);
+        let li=ce("li");
+        li.setAttribute("user-data",`File#${countFiles}`);
+        let delli =  ce("img");
+        delli.addEventListener("click", delFiles)
+        delli.setAttribute("src","img/close.png");
+        delli.setAttribute("user-data",`File#${countFiles}`);
+        li.append(delli);
+        document.querySelector("ol").append(li);
+        countFiles++
+
+    }
+
+    function delFiles() {
+       let numberOf = this.getAttribute("user-data").split(`File#`)[1]
+       let li = document.querySelectorAll("li");
+       let file= document.querySelectorAll(`div > input`)
+        for (let i=0; i<file.length;i++) {
+            if (numberOf == file[i].getAttribute("name").split(`File#`)[1]) {
+                file[i].parentElement.remove();
+            }
         }
 
-        function delFiles() {
-
-        }
+       for (let i=0; i<li.length;i++){
+           if(numberOf==li[i].getAttribute("user-data").split(`File#`)[1]){
+               li[i].remove();
+           }
+       }
+        this.parentElement.remove();
+    }
 
 }
