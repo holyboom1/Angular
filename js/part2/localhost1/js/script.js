2f1ca8774dae22d49df5d11e8c11ed3595c71edf
IHOLY <LESHA744@GMAIL.COM>
2020-09-24 21:07:01 +0300

    QWE

--- js/part2/localhost1/js/script.js
+++ js/part2/localhost1/js/script.js
@@ -1,18 +1,34 @@
 window.onload = function () {
 
     if(checkCookie("user")) {
+
         auth.remove();
-        let user_name=getCookie();
+        let user_name=getCookie("user");
         let div = document.createElement("div");
-            div.innerHTML=`Привет, ${user_name}`;
+        div.innerHTML=`Привет, ${user_name}`;
 
 
         let exit = document.createElement("span");
-            exit.addEventListener("click",exitUser);
-            exit.innerHTML=`Выйти`;
-            document.body.append(div);
-            document.body.append(exit);
+        exit.addEventListener("click",exitUser);
+        exit.innerHTML=`Выйти`;
+        document.body.append(div);
+        document.body.append(exit);
 
+
+        if (!checkCookie("views")){
+            setCookie("views","1",1);
+            let views = document.createElement("div");
+            views.innerHTML=`   количество просмотров ${getCookie("views")}`;
+            document.body.append(views);
+        }
+        else {
+            setCookie('views', `${+getCookie("views")+1}`, 1);
+            let views = document.createElement("div");
+            views.innerHTML=`   количество просмотров ${getCookie("views")}`;
+            document.body.append(views);
+        }
+
+        lastViewed();
     }
 
     else {
@@ -20,9 +36,28 @@ window.onload = function () {
 
     }
 
+    function lastViewed() {
+        let lastDate = new Date().toString();
+        setCookie(`lastTime`, `${lastDate}`);
+        let dat = document.createElement("div");
+        dat.innerHTML=`последний вход был : ${getCookie("lastTime")}`;
+        document.body.append(dat);
+    }
+    function firstDate() {
+        let first = new Date().toString();
+        setCookie(`firstTime`, `${first}`);
+        let dat = document.createElement("div");
+        dat.innerHTML=`first date : ${getCookie("firstTime")}`;
+        document.body.append(dat);
+    }
+
     function exitUser() {
         setCookie("user","", 0);
+        setCookie("views","", 0);
+        setCookie(`lastTime`, ``, 0);
+        setCookie(`firstTime`, ``, 0);
         location.reload();
+
     }
 
     function checkCookie(name) {
@@ -33,8 +68,13 @@ window.onload = function () {
         document.cookie=`${name}=${val};max-age=${expires * 3600}`;
     }
 
-    function getCookie() {
-        return document.cookie.split("=")[1];
+    function getCookie(name) {
+        let cook = document.cookie.split(";");
+        for (let i=0; i<cook.length;i++){
+            if (cook[i].split("=")[0].trim()==name){
+                return cook[i].split("=")[1];
+            }
+        }
     }
 
     function getFormData(e) {
@@ -42,6 +82,7 @@ window.onload = function () {
 
         if(!checkCookie("user")) {
             setCookie("user",document.querySelector("#user_name").value,1);
+
             location.reload();
         }
         else {
