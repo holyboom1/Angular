7b95eccbc560266a5c1666b2fc3d769bc6df43fb
IHOLY <LESHA744@GMAIL.COM>
2020-11-05 19:11:29 +0300

    QWE

--- js/react/1/src/components/sections/Footer.js
+++ js/react/1/src/components/sections/Footer.js
@@ -5,8 +5,12 @@ import InfoText from "./sectionsParts/InfoText";
 export default function Footer(props) {
     return <section className={"container"}>
     <div className={"row"}>
-        <Copyright />
+        <section className={`col-lg-6`}>
+       <Copyright />
+        </section>
+        <section className={`col-lg-6`}>
         <InfoText/>
+        </section>
     </div>
     </section>
 
